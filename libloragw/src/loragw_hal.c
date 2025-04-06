66359eaed20236d43c064e1b400097c000a871b3
Peiman Moradi <Peiman.Moradi@uwe.ac.uk>
2025-04-06 15:59:28 +0100

    1. GPIO set modified for the new rpi OS. 2. Temperature sensor disabled.

--- libloragw/src/loragw_hal.c
+++ libloragw/src/loragw_hal.c
@@ -1114,7 +1114,8 @@ int lgw_start(void) {
         }
         if (i == sizeof I2C_PORT_TEMP_SENSOR) {
             printf("ERROR: no temperature sensor found.\n");
-            return LGW_HAL_ERROR;
+            ts_fd = -1;
+            // return LGW_HAL_ERROR;
         }
 
         /* Configure ADC AD338R for full duplex (CN490 reference design) */
--- tools/reset_lgw.sh
+++ tools/reset_lgw.sh
@@ -17,40 +17,47 @@ SX1302_POWER_EN_PIN=18  # SX1302 power enable
 SX1261_RESET_PIN=22     # SX1261 reset (LBT / Spectral Scan)
 AD5338R_RESET_PIN=13    # AD5338R reset (full-duplex CN490 reference design)
 
+SX1302_RESET_GPIO=$(gpiofind GPIO${SX1302_RESET_PIN})
+SX1261_RESET_GPIO=$(gpiofind GPIO${SX1261_RESET_PIN})
+SX1302_POWER_EN_GPIO=$(gpiofind GPIO${SX1302_POWER_EN_PIN})
+AD5338R_RESET_GPIO=$(gpiofind GPIO${AD5338R_RESET_PIN})
+
+
 WAIT_GPIO() {
     sleep 0.1
 }
 
 init() {
-    # setup GPIOs
-    echo "$SX1302_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1261_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$SX1302_POWER_EN_PIN" > /sys/class/gpio/export; WAIT_GPIO
-    echo "$AD5338R_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
-
     # set GPIOs as output
-    echo "out" > /sys/class/gpio/gpio$SX1302_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1261_RESET_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/direction; WAIT_GPIO
-    echo "out" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/direction; WAIT_GPIO
+    gpioset ${SX1302_POWER_EN_GPIO}=1; WAIT_GPIO
+    gpioset ${SX1302_RESET_GPIO}=1; WAIT_GPIO
+    gpioset ${SX1302_RESET_GPIO}=0; WAIT_GPIO
+
+    gpioset ${SX1261_RESET_GPIO}=1; WAIT_GPIO
+    gpioset ${SX1261_RESET_GPIO}=0; WAIT_GPIO
+
+    gpioset ${AD5338R_RESET_GPIO}=1; WAIT_GPIO
+    gpioset ${AD5338R_RESET_GPIO}=0; WAIT_GPIO
 }
 
 reset() {
     echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
-    echo "SX1261 reset through GPIO$SX1302_RESET_PIN..."
+    echo "SX1261 reset through GPIO$SX1261_RESET_PIN..."
     echo "CoreCell power enable through GPIO$SX1302_POWER_EN_PIN..."
     echo "CoreCell ADC reset through GPIO$AD5338R_RESET_PIN..."
 
-    # write output for SX1302 CoreCell power_enable and reset
-    echo "1" > /sys/class/gpio/gpio$SX1302_POWER_EN_PIN/value; WAIT_GPIO
+    # Instead of writing to sysfs, use gpioset to control the pins
+    gpioset ${SX1302_POWER_EN_GPIO}=1; WAIT_GPIO
 
-    echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
-    echo "0" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
+    gpioset ${SX1302_RESET_GPIO}=1; WAIT_GPIO
+    gpioset ${SX1302_RESET_GPIO}=0; WAIT_GPIO
 
-    echo "0" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
-    echo "1" > /sys/class/gpio/gpio$SX1261_RESET_PIN/value; WAIT_GPIO
+    gpioset ${SX1261_RESET_GPIO}=0; WAIT_GPIO
+    gpioset ${SX1261_RESET_GPIO}=1; WAIT_GPIO
 
-    echo "0" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
+    gpioset ${AD5338R_RESET_GPIO}=0; WAIT_GPIO
+    gpioset ${AD5338R_RESET_GPIO}=1; WAIT_GPIO
+
     echo "1" > /sys/class/gpio/gpio$AD5338R_RESET_PIN/value; WAIT_GPIO
 }
 
